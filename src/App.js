0c433eca38ef88ce013978c62732ca4a0060f6b5
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-05-13 11:39:26 +0300

    Implemented Header [navBar] Separately

--- src/App.js
+++ src/App.js
@@ -2,52 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 
-import { Container, Image,  Menu, Visibility } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react'
 
 import MoviesPage from './components/pages/MoviesPage';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 import { Link, Route } from 'react-router-dom';
 
-import { menuStyle, fixedMenuStyle } from './helpers/styleHelpers';
 
 class App extends Component {
-
-	state = {
-		menuFixed: null,
-		overlayFixed: false,
-	}
-
-
-	stickTopMenu = () => this.setState({ menuFixed: true })
-	unStickTopMenu = () => this.setState({ menuFixed: null })
-
 	render() {
-		const { menuFixed } = this.state
 		return (
 			<div className="App">
-				<Visibility
-					onBottomPassed={this.stickTopMenu}
-					onBottomVisible={this.unStickTopMenu}
-					once={false}
-				>
-					<Menu
-						borderless
-						fixed={menuFixed && 'top'}
-						style={menuFixed ? fixedMenuStyle : menuStyle}
-					>
-						<Container text>
-							<Menu.Item>
-								<Image size='mini' src='https://react.semantic-ui.com/logo.png' />
-							</Menu.Item>
-							<Menu.Item header>Movieapp</Menu.Item>
-							<Menu.Item as={Link} to="/movies">
-								Movies
-              </Menu.Item>
-							<Menu.Item as='a'>Add New</Menu.Item>
-						</Container>
-					</Menu>
-				</Visibility>
+				<Header />
 				<Container text>
 					<Route path='/movies' component={MoviesPage}></Route>
 					Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
