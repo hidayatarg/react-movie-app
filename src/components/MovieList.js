0e3187d618c2cc329fb7884cfa17ab9cd385e69a
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-05-14 16:54:29 +0300

    Added Card for each Movie

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,38 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import MovieCard from './MovieCard';
+import { Grid } from 'semantic-ui-react'
 
 // stateless component
-const MovieList = ({movies}) => {
-    const emptyMessage = (
-        <p>There are no movies yet.</p>
-    );
+const MovieList = ({ movies }) => {
+  const emptyMessage = (
+    <p>There are no movies yet.</p>
+  );
 
-    const movieList = (
-        <div>
-          {
-            movies.error.response 
-            ?   <h3>Error retrieving data</h3> 
-            :   movies.movies.map(movie => <div key={movie.id}>
-                {movie.title}
-            </div>)
-          }
-        </div>
-    );
-    
-    return (
-      <div>
-        {/* Movies List */}
-        {movies.length === 0 ? emptyMessage : movieList}
-      </div>
-    )
-  
+  const movieList = (
+    <div>
+      {
+        movies.error.response
+          ? <h3>Error retrieving data</h3>
+          : movies.movies.map(movie => <MovieCard key={movie.id} movie={movie} />)
+      }
+    </div>
+  );
+
+  return (
+    <div>
+      {/* Movies List */}
+      {movies.length === 0 ? emptyMessage : movieList}
+    </div>
+  )
+
 }
 
 MovieList.prototype = {
   // state movies convert to array
-    movies: PropTypes.shape({
-      movies: PropTypes.array.isRequired,
-    }).isRequired
+  movies: PropTypes.shape({
+    movies: PropTypes.array.isRequired,
+  }).isRequired
 };
 
 
