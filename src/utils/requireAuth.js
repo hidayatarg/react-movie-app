aaf1cd661d422c7d13d3fe8bd54b5a5bfe42f8be
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-06-04 13:51:27 +0300

    Added Component Redirect From a Secured Component

--- src/utils/requireAuth.js
+++ src/utils/requireAuth.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
 export default function(ComposedComponent) {
     class Authenticate extends Component {
+        componentWillMount() {
+            if(!this.props.isAuthenticated){
+                // this.props.addFlashMessage({
+                //     type:'error',
+                //     text: 'You need to login to access this page'
+                // });
+                // redirect
+                // TODO: ! give  message in Login Page
+                console.log('gelen router: ', this.context);
+                this.props.history.push('/login');
+            }
+        }
         render() {
             console.log('gelen require Auth: ', this.props);
             
@@ -18,6 +31,9 @@ export default function(ComposedComponent) {
     Authenticate.prototypes = {
         isAuthenticated: PropTypes.bool.isRequired
     }
+    // Authenticate.contextType = {
+    //     router: PropTypes.object.isRequired
+    // }
     const mapStateToProps = ({ login }) => {
         return {
             isAuthenticated: login.isAuthenticated
